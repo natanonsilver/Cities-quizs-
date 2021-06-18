71d858a11d651682581bde0c99181ce6a1d47252
natanonsilver <78886154+natanonsilver@users.noreply.github.com>
2021-06-18 12:13:43 +1200

    Update assembled 1.py

--- Cities quiz/assembled 1.py
+++ Cities quiz/assembled 1.py
@@ -1,27 +1,41 @@
-from random import shuffle
 # In this version I will be using dictionaries and list to create my sports quiz.
-print("Welcome To General Cities Quiz")
+print("Welcome To Sports Rules and Skills Quiz")
 import datetime
 x=datetime.datetime.now()
 print(x)
-print("--------------------------------------------------------------")
-
+print("=============================================================")
+     
 #Using functions to greet the user.
 
 def greet():
     
     while True:
         name = input("What's your name? : ")
-        if name.replace(' ','').isalpha():
+        if name.isalpha():
             break
         print("Please enter characters A-Z only")
 greet()#calling the greet function
 
-
+def age():
+    while True:
+        age = input("Please enter your age : ")
+        if age.replace(' ','').isnumeric():#using replace to allow spaces after the answer
+            if 3< int(age)<100: #allowing age, 3 to 100 only
+                break
+            else:
+                print("=========================================================")
+                print('Sorry you have to between 3 and 100 to play this quiz! ')
+                print("Thank you come again!")
+                print("=========================================================")
+                exit()
+               
+        else:
+            print("The data type you have used is invalid data type.\n")
+age()
 #Asking the user to enter their age by using try and except.
 
 def instructions():
-    inst=input("Do you need instructions to play? : answer 'y' for Yes and 'n' for No :")
+    inst=input("Do you need instructions to play? : answer 'y' for Yes and 'n' for No")
     if inst=="y":
         print("=======================================================================================")
         print("You will be given a set of four or three answers and you must choose which one is right")
@@ -36,148 +50,81 @@ instructions()#calling instructions function
 
 
 #Asking the user for their status
-
-ready=input("Are you ready for the quiz?:")
+ready=input("Are you ready for the quiz?: press y to continue or x to exit:")
 
 if ready=="y" or ready == "yes" :
     print("lets continue")
     print("------------------------------------------------------------")
     print("Please answer the questions with the the alphabets (a,b,c,d)")
     print("------------------------------------------------------------")
-elif ready=="n" or ready == "No":
-    print("Come back and play agin")
-    exit()
-
-
-
 
 #Creating dictionary for question and the right answer
 
-print("                                                    ")
-#set of questions that are asked
-#question 1
-## questions and right answers
-
-def rounds():
-    global r
-    while True:
-        try:
-            r = int(input("\nPlease enter how many rounds you want to play : "))##asking the user how many rounds they want to play
-            if 0<r<=10:
-                break
-            else:
-                print("please enter the rounds in 1 to 10 only")
-        except:
-            print('please enter rounds in numbers only (The max is 10)')
+citesquiz={
+'1.What is the captial for Nigeria' : 'a',
+'2.What is the captial for Spain' :'b',
+'3.What is the captial for Canada' :'c',
+'4.What is the captial for New Zealand' :'b',
+'5.What is the captial for Italy' :'a',
+'6.What is the captial for Peru' :'c',
+'7.What is the captial for San Marino' :'a',
+'8.What is the captial for Mexico' : 'c',
+'9.What is the captial for United States Of America' : 'b',
+'10.What is the captial for Syria' :'d'
 
 
+}
 
-rounds()
+#Creating list of answer options
+optlist=[
+'a)Abuja:b)Miami:c)Belmopan:d)Jerusalem',
+'a)Astana:b)Madrid:c)Kingston:d)Bujumbura',
+'a)Philipsburg:b)Lusaka:c)Ottawa:d)Lima',
+'a)Monrovia:b)Wellington:c)Ottawa:d)Suva',
+'a)Rome:Palikir:b)Algiers:c)Seoul',
+'a)Copenhagen:b)Kuwait City:c)Lima',
+'a)San Marino:b)Pyongyang:c)Marigot:d)Canberra',
+'a)West Island:b)Bissau:c)Mexico City:d)Nouakchott',
+'a)Nairobi:b)Washington DC:c)Stanley:d)Phnom Penh',
+'a)Yaren:b)Harare:c)Palikir:d)Damascus',
+]
+index=0
+score=0
+optnum=0
+    
+        #Questions for Cites Quiz.
+for q in citesquiz.keys():
+        print(q) #Printing one question at a time in order.
+        print(optlist[index]) #Print first option.
+        index=index+1
+        print("            ")
+        
+        user_ans=input("Enter your answer:")
+        answer = citesquiz[q] # Finding answer to the question in the dictionary.
 
-
-score = 0
-questions=[
-
-[
-    "What is the capital of Nigeria?",
-    {'answer':'a','option':'\na.Abuja \nb.Miami \nc.Belmopan \nd.Jerusalem '}
-    ],
-
-[
-    "What is the captial for Spain?",
-    {'answer':'b','option':'\na.Astana \nb.Madrid \nc.Kingston \nd.Bujumbura '}
-    ],
-
-
-[
-    "What is the captial for Canada?",
-    {'answer':'c','option':'\na.Philipsburg \nb.Lusaka \nc.Ottawa \nd.Lima '}
-    ],    
-   
-[
-    "What is the captial for New Zealand?",
-    {'answer':'b','option':'\na.Monrovia\nb.Wellington \nc.Ottawa \nd.Suva '}
-    ],    
-   
-
-[
-    "What is the captial for Italy",
-    {'answer':'a','option':'\na.Rome \nb.Palikir\nc.Algiers \nd.Seoul'}
-    ],    
-     
-
-
-[
-    "What is the captial for Peru?",
-    {'answer':'c','option':'\na.Copenhagen \nb.Kuwait City\nc.Lima'}
-    ],    
-     
-[
-    "What is the captial for San Marino?",
-    {'answer':'a','option':'\na.San Marino \nb.Pyongyang \nc.Marigot \nd.Canberra'}
-    ],    
-
-[
-    "What is the captial for Mexico?",
-    {'answer':'c','option':'\na.West Island \nb.Bissau \nc.Mexico City \nd.Nouakchott '}
-    ],    
-[
-    "What is the captial for United States Of America?",
-    {'answer':'b','option':'\na.Nairobi \nb.Washington DC \nc.Stanley'}
-    ],    
-
-[
-    "What is the captial for Syria?",
-    {'answer':'d','option':'\na.Yaren \nb.Harare \nc.Palikir \nd.Damascus'}
-    ],
+        if user_ans==answer:
+            print("That is correct, Keep up the good work")
+            print("---------------------")
+            score=score+1
+            print("Score",score+0)
+        else:
+            print("That is not correct. The answer is", answer)
 
 
-        ]
-shuffle(questions)
-## start of quiz
-print("Start of quiz")
-while r>0:
-    data = questions[0]
-    q = data[0]
-    data =data[1]
-    answer = data['answer']
-    option = data['option']
+             
 
-    print(q)
-    print(option)
-    while True:
-        user_answers = input("Please enter your answer here : ").lower().replace(' ','')
-        if user_answers =='a' or user_answers == 'b' or user_answers == 'c' or user_answers == 'd':
-            if user_answers == answer:
-                print("---------------------------")
-                print("Great job, its right! ")
-                print("---------------------------")
-                score +=1
-                print("===================")
-                print("your score is",score)
-                print("===================")
-            else:
-                print("-----------------------------------------------------------------------------")
-                print("oh no sorry your answer is not correct. This is the correct answer is ",answer)    
-                print("-----------------------------------------------------------------------------")
-                print("your score is",score)
-                print("=======")
-
-                
-                print("---------------------")
+            print("---------------------")
                
+            print("Your score is",score,"out of 10 points")  #Presenting the scores to the users.
+            print("Quiz Complete, WELL DONE :)")
+
             
 
-
-            del questions[0]
-            r-=1
-            break
-        else:
-            print("please enter the alphabet for the chosen option")
-                   
-#Presenting the scores to the users.
-print("-------------------------------- QUIZ SUMMARY ----------------------------------")
-print("Your score is",score) 
-print("Quiz Complete, WELL DONE :)")
+    
+if ready== "x" :  #If the user inputs x or no, when they are asked if they want to contiue the quiz or not.
+    print("Consider playing later")
+elif ready== "no":
+    print ("Consider playing later ")
 exit()
+
 
