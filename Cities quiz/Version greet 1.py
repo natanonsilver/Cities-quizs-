31e2465bbe8fc1bcc821ce1827a50ce20bfadfb3
natanonsilver <78886154+natanonsilver@users.noreply.github.com>
2021-06-18 12:32:02 +1200

    Update Version greet 1.py

--- Cities quiz/Version greet 1.py
+++ Cities quiz/Version greet 1.py
@@ -1,19 +1,8 @@
-from random import shuffle
-# In this version I will be using dictionaries and list to create my sports quiz.
-print("Welcome To General Cites Quiz")
-import datetime
-x=datetime.datetime.now()
-print(x)
-print("--------------------------------------------------------------")
-
-#Using functions to greet the user.
-
 def greet():
     
     while True:
         name = input("What's your name? : ")
-        if name.replace(' ','').isalpha():
+        if name.isalpha():
             break
         print("Please enter characters A-Z only")
 greet()#calling the greet function
-
